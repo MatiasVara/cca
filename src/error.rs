f300215fb0170bea109da590b420e5dadd99169e
Matias Ezequiel Vara Larsen <mvaralar@redhat.com>
2025-07-24 06:58:34 -0400

    Fix Error type
    Signed-off-by: Matias Ezequiel Vara Larsen <mvaralar@redhat.com>

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use vmm_sys_util::errno;
+use kvm_ioctls::Error as IoError;
 
 #[derive(Debug)]
 pub enum Error {
-    VmCreate(errno::Error),
-    GICCreate(errno::Error),
-    GICInit(errno::Error),
-    Config(errno::Error),
-    RDCreate(errno::Error),
-    RPopulate(errno::Error),
-    RInitiate(errno::Error),
-    RActivate(errno::Error),
+    VmCreate(IoError),
+    GICCreate(IoError),
+    GICInit(IoError),
+    Config(IoError),
+    RDCreate(IoError),
+    RPopulate(IoError),
+    RInitiate(IoError),
+    RActivate(IoError),
 }
